8be66b849a8442bc54b06787f5889ae6af5c1d0e
Ty Tran <trandaity151@gmail.com>
2023-09-25 14:36:55 +0700

    add global variables

---
+++ Core/Inc/global.h
@@ -0,0 +1,15 @@
+/*
+ * global.h
+ *
+ *  Created on: Sep 25, 2023
+ *      Author: trand
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+extern int sec;
+extern int min;
+extern int hour;
+
+#endif /* INC_GLOBAL_H_ */
---
+++ Core/Src/global.c
@@ -0,0 +1,12 @@
+/*
+ * global.c
+ *
+ *  Created on: Sep 25, 2023
+ *      Author: trand
+ */
+
+#include "global.h"
+
+int sec = 0;
+int min = 0;
+int hour = 0;
